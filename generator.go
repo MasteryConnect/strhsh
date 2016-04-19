0fb327432cd2361d59598644fbbcb6bc0269ffb8
Adam Haymond <adam@masteryconnect.com>
2016-04-19 15:15:26 -0600

    changed log to return error instead

--- generator.go
+++ generator.go
@@ -2,26 +2,24 @@ package strhsh
 
 import (
 	"crypto/rand"
-
-	"github.com/apex/log"
+	"errors"
 )
 
 const AlphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const Numeric = "0123456789"
 
-func RandStr(strSize int, randType int) string {
-
+func RandStr(strSize int, randType int) (string, error) {
 	types := []string{"", "alphanum", "number", "alpha"}
 	if len(types) < randType {
-		log.Warn("invalid Random String type given for strhsh.RandStr()")
-		return ""
+		err := errors.New("invalid Random String type given for strhsh.RandStr()")
+		return "", err
 	}
 	rt := types[randType]
 	var strMap string
 	switch rt {
 	default:
-		log.Warn("incorrect type for RandStr. Choose 1-3 [alphanumeric, numeric, alpha")
+		strMap = AlphaNumeric
 	case "alphanum":
 		strMap = AlphaNumeric
 	case "number":
@@ -35,5 +33,5 @@ func RandStr(strSize int, randType int) string {
 	for k, v := range bytes {
 		bytes[k] = strMap[v%byte(len(strMap))]
 	}
-	return string(bytes)
+	return string(bytes), nil
 }
